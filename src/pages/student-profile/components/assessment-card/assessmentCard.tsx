b0f8a0b2a6eafaf74179ececb4209ce01753a9e7
bjk <44651947+beniljosek@users.noreply.github.com>
2024-09-28 01:11:16 +0530

    integrate getStudent api (#36)

--- src/pages/student-profile/components/assessment-card/assessmentCard.tsx
+++ src/pages/student-profile/components/assessment-card/assessmentCard.tsx
@@ -1,6 +1,20 @@
-const AssessmentCard = () => {
+import { FC } from 'react';
+
+import styles from './assessmentCard.module.scss';
+
+interface AssessmentCardProps {
+    title: string;
+    score: number;
+    submittedDate: string;
+}
+
+const AssessmentCard: FC<AssessmentCardProps> = ({ title, score, submittedDate }) => {
     return (
-        <div></div>
+        <div className={styles.assessmentCardContainer}>
+            <span className={styles.name}>{title}</span>
+            <span className={styles.score}>{score}/100</span>
+            <span className={styles.date}>{submittedDate}</span>
+        </div>
     );
 }
 
