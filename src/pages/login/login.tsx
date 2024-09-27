f24e264bc769695c04b320d07490ae6b4fd720c5
Sandeep <sandeep@lm-kv-sandeep.local>
2024-09-27 14:07:29 +0530

    login

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,12 +1,56 @@
-import { TextField } from '@mui/material';
+import { ChangeEvent, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const login = () => {
+import FormInput from '../../components/form-input/formInput';
+import logo from '../../assets/logo.svg';
+import Button from '../../components/button/button';
+import LoginImg from '../../assets/login-img.jpg';
+
+import styles from './login.module.scss';
+
+const Login = ({type}: {type: string}) => {
+    const navigate = useNavigate();
+    const [formData, setFormData] = useState({});
+    
+    const handleChange = (e: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>, key: string) => {
+      setFormData({
+        ...formData,
+        [key]: e.target.value
+      });
+    }
+
+    const handleLogin = () => {
+      console.log(formData);
+    }
+
+    const handlePageSwitch = () => {
+      if (type === 'login') {
+        navigate('/signup');
+      } else {
+        navigate('/login');
+      }
+    }
+  
     return (
-      <div className="flexCenter">
-        <TextField label="Username" placeholder="Enter username" type="text" />
-        <TextField label="Password" placeholder="Enter password" type="password" />
+      <div className={styles.loginOverlay} >
+      <div className={styles.loginWrapper}>
+        <div className={styles.loginHeader}>
+          <img src={logo} alt="logo" />
+          <Button onClick={handlePageSwitch} label={type === 'login' ? 'Signup': 'Login'} />
+        </div>
+        <div className={styles.formWrapper}>
+          <div className={styles.leftSection}>
+            <img src={LoginImg} alt="LoginImg" />
+          </div>
+          <div className={styles.rightSection}>
+            <FormInput label="Username" placeholder="Enter username" type="text" onChange={(e) => handleChange(e, 'Username')}/>
+            <FormInput label="Password" placeholder="Enter password" type="password" onChange={(e) => handleChange(e, 'Password')}/>
+            <Button containerClass={styles.loginBtn} onClick={handleLogin} label={type === 'login' ? 'Login': 'Signup'} />
+          </div>
+        </div>
+      </div>
       </div>
     );
 }
 
-export default login
+export default Login;
