4ce28d4a46e26842fc37ede748186862a7f1541f
Benil Jose Kaiyanickal <beniljosenellad@gmail.com>
2024-09-27 10:24:19 +0530

    implement MUI ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
 import RootLayout from "./layout/rootLayout";
+
+const muiTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#25BF8B'
+    },
+    secondary: {
+      main: '#D3EEE3'
+    }
+  }
+});
 
 function App() {
   return (
     <div className="App">
-      <RootLayout />
+      <ThemeProvider theme={muiTheme}>
+        <RootLayout />
+      </ThemeProvider>
     </div>
   );
 }
